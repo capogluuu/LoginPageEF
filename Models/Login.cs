c807394f7533667b53cec4b9b24c3b288a1c3c77
capogluuu <capogluuu@gmail.com>
2021-11-04 16:43:40 +0300

    Update Login.cs

--- Models/Login.cs
+++ Models/Login.cs
@@ -5,13 +5,34 @@ namespace LoginPage.Models
 {
     public class Login
     {
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private string Gsm { get; set; }
+        private string Adress { get; set; }
+        private string Username { get; set; }
+        private string Password { get; set; }
+        private string Status { get; set; }
+
+        public Login() { }
+        public Login(string _name, string _surname, string _gsm, string _adress, 
+                    string _username, string _password, string _status)
+        {
+            Name = _name;
+            Surname = _surname;
+            Gsm = _gsm;
+            Adress = _adress;
+            Username = _username;
+            Password = _password;
+            Status = _status;
+        }
+        
         [Key]
-        public string username { get; set; }
-        public string name { get; set; }
-        public string surname { get; set; }
-        public string gsm { get; set; }
-        public string adress { get; set; }
-        public string password { get; set; }
-        public string status { get; set; }
+        public string username { get { return Username; } set { Username = value; } }
+        public string name { get { return Name; } set { Name = value; } }
+        public string surname { get { return Surname; } set { Surname = value; } }
+        public string gsm { get { return Gsm; } set { Gsm = value; } }
+        public string adress { get { return Adress; } set { Adress = value; } }
+        public string password { get { return Password; } set { Password = value; } }
+        public string status { get { return Status; } set { Status = value; } }
     }
 }
